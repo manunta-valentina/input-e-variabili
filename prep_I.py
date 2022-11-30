ed41b0577d4c3f4e625131b0efbf95b6576e0c45
manunta-valentina <manunta.valentina.gioia@iisgalvanimi.edu.it>
2022-11-30 21:22:07 +0000

    es.i
    git commit -m es.i

---
+++ prep_I.py
@@ -0,0 +1,12 @@
+
+def chrismass_tree (n,c):
+    n_ast=1
+    n_spazio=h-1
+    for x in range (0,4) :
+        print (n_spazio * " " , n_ast * c )
+        n_ast=n_ast+2
+        n_spazio=n_spazio-1
+altezza=int(input("dimmi l'altezza"))
+char=input ("dimmi il carattere")
+chrismass_tree(altezza,charv)
+
